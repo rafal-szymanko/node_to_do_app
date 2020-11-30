843c56dfcbb0f5a625449402d7768f5e77047355
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-11-30 19:59:47 +0100

    Add command handler

---
+++ app.js
@@ -0,0 +1,30 @@
+const parseArgs = require('minimist');
+const colors = require('colors');
+
+const command = parseArgs(process.argv.slice(2,3));
+
+const prop = '_'
+
+const newCommand = Object.keys(command).reduce((object, key) => {
+    if (key !== prop) {
+      object[key] = command[key]
+    }
+    return object
+  }, {})
+
+
+const handleCommit = ({add = null, remove = null, list = null}) => {
+    
+    if(add) {
+        if(typeof add !== 'string') return console.log('You should write text'.red);
+        console.log('dodaje zadania');
+    } else if(remove) {
+        console.log("usuwam zadanie");
+    } else if(list || list === '') {
+        console.log("pokazuje liste");
+    } else {
+        console.log("Undefined operation");
+    }
+}
+
+handleCommit(newCommand);
