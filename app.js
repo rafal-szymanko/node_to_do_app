d99d9b4f4955bc98c7af858ccbc07f77b1f99825
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-12-01 19:08:09 +0100

    Add handler to add and remove task

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const parseArgs = require('minimist');
-const colors = require('colors');
+const fs = require('fs');
 
 const command = parseArgs(process.argv.slice(2,3));
 
@@ -12,22 +12,51 @@ const newCommand = Object.keys(command).reduce((object, key) => {
     return object
   }, {})
 
+  const handleData = (type, name) => {
+    const data = JSON.parse(fs.readFileSync('db.json', 'utf8'));
+
+    if(type === "list") {
+        data.map(item => console.log(item.title));
+
+    } else if(type === "add") {
+        if(data.find(item => item.title === name)) return console.log("Task already exist");
+        const id = data.length + 1;
+
+        const task = {
+            id: id,
+            title: name,
+        }
+
+        const newArr = data.concat(task);
+
+        fs.writeFileSync('db.json', JSON.stringify(newArr))
+        console.log(`Added: ${name}`);
+    } else if (type === "remove") {
+        if(data.find(item => item.title === name)) {
+            const newArr = data.filter(item => item.title !== name);
+            fs.writeFileSync('db.json', JSON.stringify(newArr));
+            console.log(`Removed: ${name}`);
+        } else {
+            console.log("Task doesn't exist");
+        }
+    }
+}
+
 
 const handleCommit = ({add = null, remove = null, list = null}) => {
     
     if(add) {
         if(typeof add !== 'string') return console.log('You should write text'.red);
-        console.log('dodaje zadania');
+        handleData("add", add)
     } else if(remove) {
-        console.log("usuwam zadanie");
         if(typeof remove !== 'string') return console.log('You should write text'.red);
-        handleData()
+        handleData("remove", remove)
     } else if(list || list === '') {
-        console.log("pokazuje liste");
-        handleData()
+        handleData("list", remove)
     } else {
         console.log("Undefined operation");
     }
 }
 
 handleCommit(newCommand);
+
